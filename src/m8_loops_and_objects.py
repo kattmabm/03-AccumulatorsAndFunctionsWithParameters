129c5cbc4ce54182c35c90bbeddd450d1388fd93
kattmabm <kattmabm@rose-hulman.edu>
2018-09-05 11:46:48 -0400

    this code empty. YEET

--- src/m8_loops_and_objects.py
+++ src/m8_loops_and_objects.py
@@ -6,8 +6,8 @@ This module demonstrates simple LOOPS of the form:
 and also USING OBJECTS.
 
 Authors: David Mutchler, Dave Fisher, Vibha Alangar, Mark Hays, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         their colleagues and Braden Kattman.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 import rosegraphics as rg
 
@@ -15,7 +15,7 @@ import rosegraphics as rg
 def main():
     """ Calls the other functions to demonstrate and/or test them. """
     # Test your functions by putting calls to them here:
-
+    draw_circles1()
 
 def print_sequence1():
     """
@@ -29,7 +29,7 @@ def print_sequence1():
        200
     """
     # ------------------------------------------------------------------
-    # TODO: 2. Implement this function, per its doc-string above.
+    # DONE: 2. Implement this function, per its doc-string above.
     # Put a statement in  main  to test this function.
     # REQUIREMENT: You must use a   RANGE  statement to solve this problem.
     # ------------------------------------------------------------------
@@ -37,6 +37,9 @@ def print_sequence1():
     print('--------------------------------------------------')
     print('Running print_sequence1:')
     print('--------------------------------------------------')
+
+    for k in range(21):
+        print(10*k)
 
 
 def draw_circles1():
@@ -58,6 +61,13 @@ def draw_circles1():
     print('Running draw_circles1:  See graphics window')
     print('--------------------------------------------------')
 
+    window = rg.RoseWindow(400, 400)
+    center_point = rg.Point(200, 200)
+    for k in range(21):
+        circle = rg.Circle(center_point, (10 * k))
+        circle.attach_to(window)
+
+    window.close_on_mouse_click()
 
 def print_sequence2():
     """
